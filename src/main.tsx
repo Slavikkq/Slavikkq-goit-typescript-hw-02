7cbcf5ee6a38cdeec31c240003a2e2a746a3f9dd
Slavikkq <yaroslavmanuljak@gmail.com>
2024-05-12 00:19:45 +0300

    typescript-hw-02

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App.jsx";
-import "modern-normalize";
+import App from "./components/App/App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
